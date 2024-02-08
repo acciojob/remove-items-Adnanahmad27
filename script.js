ccf780f07249a7205c77f30e762e72d376191d59
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-08 18:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const colorSelect = document.querySelector("#colorSelect");
+const btn = document.querySelector('input');
+btn.addEventListener("click", () => {
+	const colorSelected = colorSelect.value;
+	options = colorSelect.options;
+
+	
+	for(let i = 0 ; i < options.length ; i++){
+		
+		if(options[i].innerText === colorSelected){
+			colorSelect.remove(i);
+			break;
+		}
+	}
+});
